4945937308709d6c4d59d001876aa70075965db1
Ezhil-Sekar-ICD201-01 <166552821+Ezhil-Sekar-ICD201-01@users.noreply.github.com>
2024-09-19 13:42:49 +0000

    Ezhil

--- PP1_4.py
+++ PP1_4.py
@@ -2,16 +2,23 @@
 
 def q1():
   #Write Assignment code here
-
+  user = input("Input a word:")
+  print(user)
 def q2():
   #Write Assignment code here
-
+  user2 = "Input your first name: "
+  print("Hello "+user2)
 def q3():
   #Write Assignment code here
+  user3 = "Input your first name: " 
+  user4 = "Input your last name:"
+  print(f"{user4}{user3}") 
 
 def q4():
   #Write Assignment code here
-
+  student = "Input a student: "
+  student2 = "Input another student: "
+  "Your students are {student} and {student2}"
 #Do not edit code below this comment
 
 q1()
